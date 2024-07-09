e4f87cc2f1d1dff1b74a44a874c00312e32cdade
wondermackay <kouwonoumerveil.@gmail.com>
2024-07-09 15:20:40 +0200

    ajiut du temps de jest 5

--- __test__/albumController.test.js
+++ __test__/albumController.test.js
@@ -1,13 +1,10 @@
-
-jest.setTimeout(50000); // Augmenter le délai d'attente à 10 secondes (par défaut, c'est 5 secondes)
-
 const mongoose = require('mongoose');
 const request = require('supertest');
 const { app, server } = require('../index');
 const Albums = require('../models/Albums');
 
 beforeAll(async () => {
-  const uri = 'mongodb://mongo:27017/phototeque';
+  const uri = process.env.MONGO_URI || 'mongodb://mongo:27017/phototeque'; // Utiliser MONGO_URI de l'environnement
   if (mongoose.connection.readyState === 0) {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
@@ -21,7 +18,7 @@ beforeAll(async () => {
 afterAll(async () => {
   await mongoose.disconnect();
   if (server && server.close) {
-    server.close(); // Close the server after tests
+    server.close(); // Fermer le serveur après les tests
   }
 });
 
@@ -31,12 +28,14 @@ afterEach(async () => {
 
 describe('Album Controller', () => {
   it('should get all the albums', async () => {
+    jest.setTimeout(30000); // Augmenter le délai d'attente à 30 secondes pour ce test
     const res = await request(app).get('/albums');
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({});
   });
 
   it('should create a new album', async () => {
+    jest.setTimeout(30000); // Augmenter le délai d'attente à 30 secondes pour ce test
     const res = await request(app)
       .post('/albums/creer')
       .field('title', 'Test Album')
@@ -46,8 +45,6 @@ describe('Album Controller', () => {
     const albums = await Albums.find({});
     expect(albums.length).toBe(1);
     expect(albums[0].title).toBe('Test Album');
-
-    // Utilisation de toMatch pour vérifier que le nom de fichier se termine par 'test_image.jpg'
     expect(albums[0].images[0]).toMatch(/test_image.jpg$/);
   });
 });
